452f30d271d11fab61e615413cc30180a2d8d76b
关于 <372301467@qq.com>
2020-06-29 11:13:22 +0800

    v1.0.0

--- example/src/index.js
+++ example/src/index.js
@@ -1,17 +1,26 @@
 const Path = require('path')
-const { HandleRequest, Server, Constant } = require('kuaizi')
+const { HandleRequest, Server, Constant, Exception } = require('kuaizi')
 const writeStaticFile = require('kuaizi-dev-static')
-const writeUploadedFile = require('../../src/index')
+const UploadedFileWriter = require('../../src/index')
 
+// 启动服务器
 Server.start()
 
-const staticPosition = Path.join(__dirname, './static')
+// 处理请求：网页文件下载
+const staticPosition = Path.join(__dirname, './static') // html 所在位置
 HandleRequest.get('/static', ctx => {
   writeStaticFile(ctx.req, ctx.res, staticPosition)
   return Constant.Nothing
 })
 
-const uploadedPosition = Path.join(__dirname, './uploaded')
+// 处理请求：上传文件
+const uploadedPosition = Path.join(__dirname, './uploaded') // 存储上传文件的位置
+const writer = new UploadedFileWriter(uploadedPosition) // 实例化 kuaizi-uploader（禁止在 handler 里实例化）
 HandleRequest.post('/upload', async ctx => {
-  await writeUploadedFile(ctx.req, uploadedPosition + '/haha') // 这里不要轻易使用 Path.join 来连接
+  const suffix = ctx.getQuery().suffix // 需要以某种方式把文件的后缀名传过来
+  if(!suffix)
+    throw new Exception.NBug('未知错误', null, '前端上传文件时，未传入文件后缀')
+
+  const filename = new Date().getTime() + '.' + suffix // 生产环境请使用更严谨的文件命名方式
+  await writer.write(ctx.req, filename)
 })
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 const FS = require('fs')
+const Path = require('path')
 
-module.exports = function(req, targetPath){
-  return new Promise( resolve => {
-    req.pipe(FS.createWriteStream(targetPath, { flags: 'wx' }))
-    req.on('end', resolve)
-  })
+module.exports = class {
+  constructor(dir){
+    if(!Path.isAbsolute(dir)) // 检查一：绝对路径
+      throw Error('请使用绝对路径')
+    FS.stat(dir, (err, stat) => {
+      if(err) // 检查二：文件夹不存在等
+        throw err
+      if(!stat.isDirectory()) // 检查三：是文件夹？
+        throw Error(dir + '不是一个文件夹？')
+
+      if(dir[dir.length-1] != '/') // 补充后尾的斜线
+        dir += '/'
+
+      this.dir = dir
+    })
+  }
+
+  write(req, filename){
+    return new Promise( resolve => {
+      const path = this.dir + filename
+      req.pipe(FS.createWriteStream(path, { flags: 'wx' }))
+      req.on('end', resolve)
+    })
+  }
 }
